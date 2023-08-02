817036ad4bb6e4adb379e14062b8a54fb83eb68d
huangxin <xin.polaris.dev@gmail.com>
2023-08-02 20:50:28 +0800

    feat：v4l2推流下预览

--- Qplayer2Demo/app/src/main/java/com/qiniu/qplayer2/RtmpConfig.java
+++ Qplayer2Demo/app/src/main/java/com/qiniu/qplayer2/RtmpConfig.java
@@ -4,6 +4,6 @@ package com.qiniu.qplayer2;
  * Created by HuangXin on 2022/12/8.
  */
 public class RtmpConfig {
-    public static final String url = "rtmp://122.224.95.114:2045/HX/HX1208";
+    public static final String url = "rtmp://115.238.101.20:2045/HX/HX1208";
 
 }
